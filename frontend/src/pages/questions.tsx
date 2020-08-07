e73d4809de7e3f685b1f28a9cd73c7dda0c1deef
Robert Plant <rob@robertplant.io>
2020-08-07 22:51:59 +0100

    Added score calculation with local state storage.

--- frontend/src/pages/questions.tsx
+++ frontend/src/pages/questions.tsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
 
 const { REACT_APP_API_URI, REACT_APP_API_URI_QUESTIONS } = process.env;
 
@@ -23,6 +28,12 @@ function App() {
   const [data, setData] = useState<QuestionEntries[] | null>(null);
   const [loading, setLoading] = useState(true);
   const [question, setQuestion] = useState(0);
+  const [score, setScore] = useState<number>(0);
+  const [value, setValue] = React.useState(null);
+
+  const handleChange = (event: any) => {
+    setValue(event.target.value);
+  };
 
   useEffect(() => {
     fetchData(url, setData, setLoading);
@@ -39,8 +50,45 @@ function App() {
   if (question < data.length) {
     return (
       <>
-        <div>{data[question].Question}</div>
-        <Button onClick={() => setQuestion(question + 1)}>Next</Button>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">{data[question].Question}</FormLabel>
+          <RadioGroup
+            aria-label="question"
+            name="question"
+            value={value}
+            onChange={handleChange}
+          >
+            <FormControlLabel
+              value="6"
+              control={<Radio />}
+              label={data[question]["6 points"]}
+            />
+            <FormControlLabel
+              value="4"
+              control={<Radio />}
+              label={data[question]["4 points"]}
+            />
+            <FormControlLabel
+              value="2"
+              control={<Radio />}
+              label={data[question]["2 points"]}
+            />
+            <FormControlLabel
+              value="0"
+              control={<Radio />}
+              label={data[question]["0 points"]}
+            />
+          </RadioGroup>
+        </FormControl>
+        <Button
+          onClick={() => {
+            setQuestion(question + 1);
+            setScore(score + parseInt((value || '0'), 10));
+            setValue(null);
+          }}
+        >
+          Next
+        </Button>
       </>
     );
   }
@@ -48,6 +96,7 @@ function App() {
   return (
     <>
       <div>Results</div>
+      <div>Score {score}</div>
     </>
   );
 }
