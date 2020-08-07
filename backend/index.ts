ae10fdfd9106091c16eb6e87f19e6359ca76c810
Robert Plant <rob@robertplant.io>
2020-08-07 21:43:03 +0100

    Allowed cors requests through.

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import {Request, Response} from "express";
+import cors from "cors";
 
 import parse from "./src/read";
 
 const app = express();
 const { PORT } = process.env;
+
+app.use(cors());
 
 app.get("/questions", (_req: Request, res: Response) => {
   res.send(parse());
